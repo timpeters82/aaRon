50f81931b6093be5e71515fd3db072a891b5574e
Aaron Statham <aaron.l.statham@gmail.com>
2014-09-29 15:11:46 +1000

    Removed redundant args from strip

--- R/utils.R
+++ R/utils.R
@@ -3,19 +3,16 @@
 #' Cleans up a \code{GRanges} object
 #'
 #' @param x A \code{GRanges} objext
-#' @param genome \code{BSgenome} object
-#' @param chrs Chromosomes to limit \code{x} to
-#' @return Reduced \code{GRanges} of restricted chromosomes, with no strand or element metadata
+#' @return Reduced \code{GRanges} with no strand or element metadata
 #'
 #' @export
 #'
-#' @importFrom GenomicRanges seqnames seqlevels<- reduce
+#' @importFrom GenomicRanges reduce
 #' @importFrom IRanges endoapply
 #'
 #' @author Aaron Statham <a.statham@@garvan.org.au>
-strip <- function(x, genome, chrs=1:24) {
+strip <- function(x) {
     if (class(x)=="GRangesList") return(endoapply(x, strip))
-    seqlevels(x, force=TRUE) <- seqnames(genome)[chrs]
     reduce(unstrand(unvalue(x)))
 }
 
